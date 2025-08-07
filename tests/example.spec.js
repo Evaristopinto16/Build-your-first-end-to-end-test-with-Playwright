f5132d0db697d35762eb64e3140713db9a05fb00
Evaristopinto16 <evaristopinto16@gmail.com>
2025-08-07 11:29:37 +0300

    add get started link na suit de test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,3 +6,9 @@ test("titulo encontrado", async ({page})=>{
 
   await expect(page).toHaveTitle(/Playwright/)
 })
+
+test("get started link",async ({page})=>{
+ await page.goto('https://playwright.dev/');
+ await page.getByRole("link", {name: "Get started"}).click();
+ await expect(page.getByRole("heading", {name: "Installation"})).toBeVisible();
+})
